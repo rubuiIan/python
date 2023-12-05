644124edb9e6f6eae871069a5d56a7a98413829d
ANGELA KARIUKI <iyanrubui@gmail.com>
2023-12-05 10:49:35 +0300

    updated python work

--- collection.py
+++ collection.py
@@ -36,10 +36,10 @@ Collection Properties
     Assignmet
     How to add an item at the start of the list_a
     FINDING THE LENGTH OF A LIST
-    -We use the inbuilt functio len() and give it our list as its argument
+        -We use the inbuilt functio len() and give it our list as its argument
     Eg: len(list_a): This will return 4 as list_a has 4 items 
-            max(list_a)- This returns the largest item in our list
-            min(list_a)- This returns the smallest item in ourn list
+        max(list_a)- This returns the largest item in our list
+        min(list_a)- This returns the smallest item in ourn list
 '''
 
 # Adding items at the end of our list
@@ -77,3 +77,75 @@ print(list_example)
 # We iterets through our list using the  for .. in loop
 for item in list_example:
     print(f"Item {item}")
+    
+# Adding an item at the beiginning of oor list
+list_example.insert(0, "fifty")
+print(f"MODIFIED LIST={list_example}")
+
+# SET OPERATIONS
+first_set = {1 ,2, 3, 4, 5, 6}
+second_set = {4, 5, 6, 7, 8, 9}
+
+#Union: Combine list
+combined_set = first_set.union(second_set)
+print(combined_set)
+
+alternative_combined_set = first_set | second_set
+print(alternative_combined_set)
+
+# Itersection: Takes the coomon item in sets
+my_intersection = first_set.intersection(second_set)
+print(my_intersection)
+
+alternative_intersection = first_set & second_set
+print(alternative_intersection)
+
+# Set difference: This is used to check items that are in one list and not in the other
+# Eg the example below, this will check for items in the first that are not in the second 
+set_difference = first_set.difference(second_set)
+print(set_difference)
+
+alternative_set_difference = second_set - first_set
+print(alternative_set_difference)
+
+
+# python Dictionaries: They store items with keys and values
+student1 = {
+  "firstname": "Johnte",
+  "secondname": "Kimani", 
+  "age": 25,
+  "is_married": False
+}
+
+student2 = {
+  "firstname": "Carl",
+  "secondname": "Wanjira", 
+  "age": 25,
+  "is_married": True
+}
+print(student1["firstname"])
+
+students = [student1, student2]
+for student in students:
+  print(f"FIRST NAME={student['firstname']}, SECOND NAME={student['secondname']}")
+  
+# Dictionary operations
+# 1, Addition
+student1["id"] = 190
+
+print(student1)
+
+# 2. Getting/Reading our dictionary
+student_id = student1["id"]
+print(student_id)
+
+alternative_student_id = student1.get("id")
+print(alternative_student_id)
+
+# Updating 
+student1["id"] = 9001
+print(student1["id"])
+
+# Delting an item from a dictionary 
+student1.pop("id")
+print(student1)
---
+++ wether.py
@@ -0,0 +1,1 @@
+import daytime as dt
